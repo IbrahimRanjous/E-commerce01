7500afc1c8e60acf661935994d4f48d7a2ca6356
ibrahim <ibrahim.ranjous@gmail.com>
2025-03-25 19:41:16 +0300

    create search container ui

--- lib/Features/home/views/home_view.dart
+++ lib/Features/home/views/home_view.dart
@@ -3,15 +3,17 @@ import 'package:iconsax/iconsax.dart';
 import 'package:rjs_store/core/utils/constants/colors.dart';
 import 'package:rjs_store/core/utils/constants/sizes.dart';
 import 'package:rjs_store/core/utils/device/device_utility.dart';
+import 'package:rjs_store/core/utils/helpers/helper_functions.dart';
 import 'widgets/home_app_bar.dart';
 import 'widgets/primary_header_container.dart';
+import 'widgets/search_container.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -20,30 +22,12 @@ class HomeView extends StatelessWidget {
               child: Column(
                 children: [
                   // -- Appbar
-                  const THomeAppBar(),
-                  const SizedBox(height: TSizes.defaultSpace),
+                  THomeAppBar(),
+                  SizedBox(height: TSizes.defaultSpace),
 
                   // -- Searchbar
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: TSizes.defaultSpace),
-                    child: Container(
-                      width: TDeviceUtils.getScreenWidth(context),
-                      padding: const EdgeInsets.all(TSizes.md),
-                      decoration: BoxDecoration(
-                        borderRadius:
-                            BorderRadius.circular(TSizes.cardRadiusLg),
-                        border: Border.all(color: TColors.grey),
-                      ),
-                      child: const Row(
-                        children: [
-                          Icon(
-                            Iconsax.search_normal,
-                            color: TColors.grey,
-                          ),
-                        ],
-                      ),
-                    ),
+                  TSearchContainer(
+                    text: 'Search In Store',
                   ),
 
                   // -- Categories
---
+++ lib/Features/home/views/widgets/search_container.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:rjs_store/Features/login/views/login_view.dart';
+import '../../../../core/utils/constants/colors.dart';
+import '../../../../core/utils/constants/sizes.dart';
+import '../../../../core/utils/device/device_utility.dart';
+import '../../../../core/utils/helpers/helper_functions.dart';
+
+class TSearchContainer extends StatelessWidget {
+  const TSearchContainer({
+    super.key,
+    required this.text,
+    this.icon = Iconsax.search_normal,
+    this.showBackground = true,
+    this.showBorder = true,
+  });
+
+  final String text;
+  final IconData? icon;
+  final bool showBackground, showBorder;
+
+  @override
+  Widget build(BuildContext context) {
+    final darkMode = THelperFunctions.isDarkMode(context);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
+      child: Container(
+        width: TDeviceUtils.getScreenWidth(context),
+        padding: const EdgeInsets.all(TSizes.md),
+        decoration: BoxDecoration(
+          color: showBackground
+              ? (darkMode ? TColors.black : TColors.light)
+              : Colors.transparent,
+          borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
+          border: showBorder
+              ? (darkMode
+                  ? Border.all(color: TColors.borderPrimary)
+                  : Border.all(color: TColors.grey))
+              : Border.all(color: Colors.transparent),
+        ),
+        child: Row(
+          children: [
+            Icon(
+              icon,
+              color: TColors.darkerGrey,
+            ),
+            const SizedBox(
+              width: TSizes.spaceBtwItems,
+            ),
+            Text(
+              text,
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
